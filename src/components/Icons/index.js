3f74016a184326c4f04ac8c5883a6d9bbf92ad1e
vishwa-akshat <akvs0707@gmail.com>
2022-02-26 18:50:16 +0530

    chore: seperated components and container, and added svgs

---
+++ src/components/Icons/index.js
@@ -0,0 +1,5 @@
+import DiceIcon from "./svgs/DiceIcon";
+import DividerPatternDesktopIcon from "./svgs/DividerPatternDesktopIcon";
+import DividerPatternMobileIcon from "./svgs/DividerPatternMobileIcon";
+
+export { DiceIcon, DividerPatternDesktopIcon, DividerPatternMobileIcon };
